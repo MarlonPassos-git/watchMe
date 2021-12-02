15faccb457977207d0534a1ff863f2a46fc67054
marlon Felipe Passos <1marlonpassos@gmail.com>
2021-12-02 12:46:18 -0300

    ajustando um pouco o layout para nao quebrar em telas pequenas

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { useEffect, useState } from 'react';
-
-
 import { MovieCard } from './components/MovieCard';
 
 // import { SideBar } from './components/SideBar';
- import { Content } from './components/Content';
-
-import { api } from './services/api';
+import { Content } from './components/Content';
 
 import './styles/global.scss';
 import './styles/sidebar.scss';
+import { SideBar } from './components/SideBar';
 
 
 interface MovieProps {
@@ -36,45 +33,25 @@ export function App() {
   const [movies, setMovies] = useState<MovieProps[]>([]);
   const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
 
-  useEffect(() => {
-    api.get<GenreResponseProps[]>('genres').then(response => {
-      setGenres(response.data);
-    });
-  }, []);
-
-  useEffect(() => {
-    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
-      setMovies(response.data);
-    });
-
-    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
-      setSelectedGenre(response.data);
-    })
-  }, [selectedGenreId]);
-
+  
   
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <Content 
-        genres={genres} 
-        setSelectedGenreId={setSelectedGenreId}
-        selectedGenreId={selectedGenreId}
-      />
-
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
+		<Content 
+		genres={genres} 
+		setSelectedGenreId={setSelectedGenreId}
+		selectedGenreId={selectedGenreId}
+		/>
+		<SideBar
+			  setGenres={setGenres}
+			  selectedGenreId={selectedGenreId}
+			  setMovies={setMovies}
+			  setSelectedGenre={setSelectedGenre}
+			  selectedGenre={selectedGenre}
+			  movies={movies}
+		/>
+      
     </div>
   )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,65 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect } from "react";
+import { api } from "../services/api";
+import { MovieCard } from "./MovieCard";
+
+interface GenreResponseProps {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+  
+interface SideBarProps {
+  setGenres: ([]: GenreResponseProps[]) => void;
+  selectedGenreId: number;
+  setMovies: ([]: MovieProps[]) => void;
+  setSelectedGenre: ({ }: GenreResponseProps) => void;
+  selectedGenre: GenreResponseProps;
+  movies: MovieProps[];
+}
+
+export function SideBar({ setGenres, selectedGenreId, movies, setMovies, setSelectedGenre ,selectedGenre}: SideBarProps) {
+  
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+		setMovies(response.data);
+		
+    });
+
+    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+    setSelectedGenre(response.data);
+    })
+  }, [selectedGenreId]);
+
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
 }
